b20282598af3730f2103c33800654455e8689fe5
Sergio Gonzalez <sgonzalez.cna@gmail.com>
2023-05-19 07:30:59 -0500

    Se organiza update todo

--- src/todo/dtos/todo/updateTodo.dto.ts
+++ src/todo/dtos/todo/updateTodo.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTodoDto {
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   @ApiProperty({ description: 'Descripcion de la tarea' })
   todo: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ description: 'Booleando para saber si la tarea esta completa' })
+  isCompleted: boolean;
 }
